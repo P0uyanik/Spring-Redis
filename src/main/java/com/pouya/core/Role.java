f9c23e1f93cade9fa46ecc63b2d39b3627a4ee82
Pouya Nikzadaghdas <puyaniek@gmail.com>
2024-01-09 19:31:32 +0100

    The Role class has been defined, which includes an ID and roles as String .

---
+++ src/main/java/com/pouya/core/Role.java
@@ -0,0 +1,28 @@
+package com.pouya.core;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
+
+import java.io.Serializable;
+
+
+@RedisHash
+public class Role implements Serializable {
+    @Id
+    private String id ;
+    @Indexed
+    private String role ;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
