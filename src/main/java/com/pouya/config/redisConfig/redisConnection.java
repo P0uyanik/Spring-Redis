4a1030309a76717a69f5fe7636bdd4461736b251
Pouya Nikzadaghdas <puyaniek@gmail.com>
2024-01-09 04:16:50 +0100

    Jedis has been chosen as the connector, and its configurations have been set inside the class.

---
+++ src/main/java/com/pouya/config/redisConfig/redisConnection.java
@@ -0,0 +1,27 @@
+package com.pouya.config.redisConfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+
+@Configuration
+@EnableRedisRepositories
+public class redisConnection {
+
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName("127.0.0.1");
+        redisStandaloneConfiguration.setPort(6379);
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
+    }
+
+
+
+}
