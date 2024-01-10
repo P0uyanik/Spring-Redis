24ac657a9cf4abed9309d67ea1b76158180a2236
Pouya Nikzadaghdas <puyaniek@gmail.com>
2024-01-10 12:05:50 +0100

    I wrote UserRepository to access the database.

---
+++ src/main/java/com/pouya/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.pouya.repository;
+import com.pouya.core.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepository extends CrudRepository<User , String> {
+    User findFirstByEmail(String email) ;
+}
