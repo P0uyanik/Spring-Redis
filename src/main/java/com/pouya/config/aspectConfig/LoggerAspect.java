1842ddde83275c594d9a2c6c3bed3d3c8f25e5c2
Pouya Nikzadaghdas <puyaniek@gmail.com>
2024-01-12 18:10:09 +0100

    Aspect Class and methods

--- src/main/java/com/pouya/config/aspectConfig/LoggerAspect.java
+++ src/main/java/com/pouya/config/aspectConfig/LoggerAspect.java
@@ -1,7 +1,24 @@
 package com.pouya.config.aspectConfig;
 
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
+@Aspect
+@Slf4j
 public class LoggerAspect {
+    @Before("execution(* com.pouya.controller.UserController.addNewRole(..))")
+    public void beforeExecution()
+    {
+        log.error("hier ist beforeExecution");
+    }
+    @After("execution(* com.pouya.controller.UserController.addNewRole(..))")
+    public void afterExecution()
+    {
+        log.error("hier ist afterExecution");
+    }
 }
