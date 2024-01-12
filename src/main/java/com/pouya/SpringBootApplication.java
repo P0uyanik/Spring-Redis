e3bf0a7a0fa96c466252940dffd7ede4c0b68e04
Pouya Nikzadaghdas <puyaniek@gmail.com>
2024-01-12 18:10:36 +0100

    EnableAspectJAutoProxy for Aspects

---
+++ src/main/java/com/pouya/SpringBootApplication.java
@@ -0,0 +1,17 @@
+
+package com.pouya ;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@org.springframework.boot.autoconfigure.SpringBootApplication
+@EnableAspectJAutoProxy
+public class SpringBootApplication  {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApplication.class, args);
+    }
+
+}
+
+
